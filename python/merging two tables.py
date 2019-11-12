bc767fabe4802b7188de050d3fda16a3a77684b7
abhishek <tiwariabhishek697@gmail.com>
2019-11-12 13:25:12 +0530

    first commit

---
+++ python/merging two tables.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[6]:
+
+
+df1 = pd.DataFrame({'HPI':[80,85,88,85],
+                    'Int_rate':[2, 3, 2, 2],
+                    'US_GDP_Thousands':[50, 55, 65, 55]},
+                   index = [2001, 2002, 2003, 2004])
+print(df1)
+
+
+# In[7]:
+
+
+
+df2 = pd.DataFrame({'HPI':[80,85,88,85],
+                    'Int_rate':[2, 3, 2, 2],
+                    'US_GDP_Thousands':[50, 55, 65, 55]},
+                   index = [2005, 2006, 2007, 2008])
+print(df2)
+
+
+# In[8]:
+
+
+df3 = pd.DataFrame({'HPI':[80,85,88,85],
+                    'Unemployment':[7, 8, 9, 6],
+                    'Low_tier_HPI':[50, 52, 50, 53]},
+                   index = [2001, 2002, 2003, 2004])
+print(df3)
+
+
+# In[5]:
+
+
+print(pd.merge(df1,df3, on='HPI'))
+
+
+# In[ ]:
+
+
+
+

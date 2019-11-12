4881eed4407d0641c44f3871c9e3d36a766bd0d9
tiwariabhishek697 <47290000+tiwariabhishek697@users.noreply.github.com>
2019-11-12 14:22:01 +0530

    second commit

--- python/merging two tables.py
+++ python/merging two tables.py
@@ -10,9 +10,9 @@ import pandas as pd
 # In[6]:
 
 
-df1 = pd.DataFrame({'HPI':[80,85,88,85],
-                    'Int_rate':[2, 3, 2, 2],
-                    'US_GDP_Thousands':[50, 55, 65, 55]},
+df1 = pd.DataFrame({'C':[80,85,88,85],
+                    'C12':[2, 3, 2, 2],
+                    'C13':[50, 55, 65, 55]},
                    index = [2001, 2002, 2003, 2004])
 print(df1)
 
@@ -21,9 +21,9 @@ print(df1)
 
 
 
-df2 = pd.DataFrame({'HPI':[80,85,88,85],
-                    'Int_rate':[2, 3, 2, 2],
-                    'US_GDP_Thousands':[50, 55, 65, 55]},
+df2 = pd.DataFrame({'C':[80,85,88,85],
+                    'C22':[2, 3, 2, 2],
+                    'C23':[50, 55, 65, 55]},
                    index = [2005, 2006, 2007, 2008])
 print(df2)
 
@@ -31,9 +31,9 @@ print(df2)
 # In[8]:
 
 
-df3 = pd.DataFrame({'HPI':[80,85,88,85],
-                    'Unemployment':[7, 8, 9, 6],
-                    'Low_tier_HPI':[50, 52, 50, 53]},
+df3 = pd.DataFrame({'C':[80,85,88,85],
+                    'C32':[7, 8, 9, 6],
+                    'C33':[50, 52, 50, 53]},
                    index = [2001, 2002, 2003, 2004])
 print(df3)
 
@@ -41,7 +41,7 @@ print(df3)
 # In[5]:
 
 
-print(pd.merge(df1,df3, on='HPI'))
+print(pd.merge(df1,df3, on='C'))
 
 
 # In[ ]:
